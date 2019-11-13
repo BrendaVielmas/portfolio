c0be41bb81bdc5e1c35802423e1c2246bb95b9cd
BrendaVielmas <breen.vielmas@gmail.com>
2019-11-12 18:59:25 -0600

    arreglo vista ipad

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,6 +4,14 @@ import '../allcss/ihover.css';
 import { scrollTo } from './Functions';
 import btnHome from '../images/topbtn.png';
 import contact from '../images/contactcomp.jpg';
+import slider1 from '../images/slider1.jpg';
+import slider2 from '../images/slider2.jpg';
+import slider3 from '../images/slider3.jpg';
+import slider4 from '../images/slider4.jpg';
+import slider5 from '../images/slider5.jpg';
+import slider6 from '../images/slider6.jpg';
+import slider7 from '../images/slider7.jpg';
+import slider8 from '../images/slider8.jpg';
 import Writer from './Writer';
 
 
@@ -52,19 +60,46 @@ class Menu extends React.Component {
         <div id="menuSection" className="Menu">
           <img className="invisible" onClick={this.scrollTopHome} id="imgTop" src={btnHome}/>
           <div className="name"><h1> Brenda Vielmas </h1></div>
-          <div className="hexagon">
+          <div className="hexagonsSec">
             <div className='one'>
            
               <div  className='otherDiv'> 
                 <Writer title="Hello" strings={[
-                  'I am a<i>Front-End Developer</i>',
-                  'I am a<strong>Woman</strong>',
-                  'I am Breen;'
+                  'I am a <strong>Front-End</strong>',
+                  'I am a <strong>Woman</strong>',
+                  'I am <strong>Breen</strong>'
                 ]}/>
               </div>
               <div class="ih-item circle visibleDiv effect13 from_left_and_right">
-                <a href="#" onClick={this.scrollToFunctionAbout}>
-                <div class="img"><img src={contact} alt="img"></img></div>
+                <a href="#" onClick={this.scrollToFunctionAbout} >
+                <div class="img">About
+                <div className="slider">
+                  <figure>
+                    <img src={slider1} alt=""></img>
+                  </figure>
+                  <figure>
+                    <img src={slider2} alt=""></img>
+                  </figure>
+                  <figure>
+                    <img src={slider3} alt=""></img>
+                  </figure>
+                  <figure>
+                    <img src={slider4} alt=""></img>
+                  </figure>
+                  <figure>
+                    <img src={slider5} alt=""></img>
+                  </figure>
+                  <figure>
+                    <img src={slider6} alt=""></img>
+                  </figure>
+                  <figure>
+                    <img src={slider7} alt=""></img>
+                  </figure>
+                  <figure>
+                    <img src={slider8} alt=""></img>
+                  </figure>
+                </div>
+                </div>
                   <div class="info">
                     <div class="info-back"> 
                       <h3>About</h3>
@@ -78,7 +113,35 @@ class Menu extends React.Component {
               <div className="skillsSec">
                 <div class="ih-item circle visibleDiv effect13 from_left_and_right">
                   <a href="#" onClick={this.scrollToFunctionSkills}>
-                  <div class="img"><img src={contact} alt="img"></img></div>
+                  <div class="img">Skills
+                    <div className="slider">
+                    <figure>
+                        <img src={slider2} alt=""></img>
+                      </figure>
+                      <figure>
+                        <img src={slider3} alt=""></img>
+                      </figure>
+                      <figure>
+                        <img src={slider4} alt=""></img>
+                      </figure>
+                      <figure>
+                        <img src={slider5} alt=""></img>
+                      </figure>
+                      <figure>
+                        <img src={slider6} alt=""></img>
+                      </figure>
+                      <figure>
+                        <img src={slider7} alt=""></img>
+                      </figure>
+                      <figure>
+                        <img src={slider8} alt=""></img>
+                      </figure>
+                      <figure>
+                        <img src={slider1} alt=""></img>
+                      </figure>
+                      
+                    </div>
+                  </div>
                     <div class="info">
                       <div class="info-back"> 
                         <h3>Skills</h3>
@@ -90,15 +153,43 @@ class Menu extends React.Component {
               </div>
               <div className='otherDiv'>
                 <Writer  title="It's" strings={[
-                  'I am a<i>Front-End Developer</i>',
-                  'I am a<strong>Woman</strong>',
-                  'I am Breen;'
+                  'I am a <strong>Woman</strong>',
+                  'I am <strong>Breen<strong>',
+                  'I am a <strong>Front-End</strong>'
                 ]}/>
               </div>
               <div className="projectsSec">
               <div class="ih-item circle visibleDiv effect13 from_left_and_right">
                     <a href="#" onClick={this.scrollToFunctionProjects}>
-                    <div class="img"><img src={contact} alt="img"></img></div>
+                    <div class="img">Projects
+                      <div className="slider">
+                      <figure>
+                          <img src={slider3} alt=""></img>
+                        </figure>
+                        <figure>
+                          <img src={slider4} alt=""></img>
+                        </figure>
+                        <figure>
+                          <img src={slider5} alt=""></img>
+                        </figure>
+                        <figure>
+                          <img src={slider6} alt=""></img>
+                        </figure>
+                        <figure>
+                          <img src={slider7} alt=""></img>
+                        </figure>
+                        <figure>
+                          <img src={slider8} alt=""></img>
+                        </figure>
+                        <figure>
+                          <img src={slider1} alt=""></img>
+                        </figure>
+                        <figure>
+                          <img src={slider2} alt=""></img>
+                        </figure>
+                       
+                      </div>
+                    </div>
                       <div class="info">
                         <div class="info-back"> 
                           <h3>Projects</h3>
@@ -112,7 +203,34 @@ class Menu extends React.Component {
             <div className='three'>
               <div class="ih-item circle visibleDiv effect13 from_left_and_right">
                 <a href="#" onClick={this.scrollToFunctionContact}>
-                <div class="img"><img src={contact} alt="img"></img></div>
+                <div class="img">Contact
+                  <div className="slider">
+                  <figure>
+                      <img src={slider4} alt=""></img>
+                    </figure>
+                    <figure>
+                      <img src={slider5} alt=""></img>
+                    </figure>
+                    <figure>
+                      <img src={slider6} alt=""></img>
+                    </figure>
+                    <figure>
+                      <img src={slider7} alt=""></img>
+                    </figure>
+                    <figure>
+                      <img src={slider8} alt=""></img>
+                    </figure>
+                    <figure>
+                    <img src={slider1} alt=""></img>
+                  </figure>
+                  <figure>
+                    <img src={slider2} alt=""></img>
+                  </figure>
+                  <figure>
+                    <img src={slider3} alt=""></img>
+                  </figure>
+                  </div>
+                </div>
                   <div class="info">
                     <div class="info-back"> 
                       <h3>Contact</h3>
@@ -123,9 +241,9 @@ class Menu extends React.Component {
               </div>
               <div className='otherDiv'>
               <Writer  title="Me" strings={[
-                'I am a<i>Front-End Developer</i>',
-                'I am a<strong>Woman</strong>',
-                'I am Breen;'
+                'I am <strong>Breen</strong>',
+                'I am a <strong>Front-End</strong>',
+                'I am a <strong>Woman</strong>'
                 ]}/>
               </div>
             </div>
