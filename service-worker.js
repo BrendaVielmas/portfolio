e7a4db0d72c449ca91c087702fc31b85f14fb03a
BrendaVielmas <breen.vielmas@gmail.com>
2019-11-22 14:43:51 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3dc2b43d3d7653e258cf2896d2c66c81.js"
+  "/portfolio/precache-manifest.41db48d7ca2c3df538b4c8232f5338dd.js"
 );
 
 self.addEventListener('message', (event) => {
