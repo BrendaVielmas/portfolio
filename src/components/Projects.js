64025934d71ad47763c8c586a7e1cae106d957a5
BrendaVielmas <breen.vielmas@gmail.com>
2019-11-25 17:13:52 -0600

    cambio de rutas para proyectos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,8 +19,8 @@ class Projects extends React.Component {
                 <div className="front"><img src={Note}></img></div>
                 <div className="back"><h1>NOT-E:</h1> a very friendly app to save all your notes.
                   <div>
-                    <div className="showHover demo"><a href="https://brendavielmas.github.io/GDL003-lab-notes/"><p>Demo</p></a></div>
-                    <div className="showHover github"><a href="https://github.com/BrendaVielmas/GDL003-lab-notes"><p>Github</p></a></div>
+                    <div className="showHover demo"><a href="https://brendavielmas.github.io/Not-E/"><p>Demo</p></a></div>
+                    <div className="showHover github"><a href="https://github.com/BrendaVielmas/Not-E"><p>Github</p></a></div>
                   </div>
                 </div>
               </div>
@@ -30,8 +30,8 @@ class Projects extends React.Component {
                 <div className="front"><img src={Econix}></img></div>
                 <div className="back"><h1>ECONIX:</h1> the best social network to change, buy or recommendate things about the ecology.
                   <div>
-                    <div className="showHover demo"><a href="https://brendavielmas.github.io/Social-Network/src/"><p>Demo</p></a></div>
-                    <div className="showHover github"><a href="https://github.com/BrendaVielmas/Social-Network"><p>Github</p></a></div>
+                    <div className="showHover demo"><a href="https://brendavielmas.github.io/Econix/src/"><p>Demo</p></a></div>
+                    <div className="showHover github"><a href="https://github.com/BrendaVielmas/Econix"><p>Github</p></a></div>
                   </div>
                 </div>
               </div>
@@ -52,7 +52,7 @@ class Projects extends React.Component {
               <div className="front"><img src={Piammy}></img></div>
                 <div className="back"><h1>PIAMMY:</h1> We can really teach through technology. Interactive piano to kids.
                   <div>
-                    <div className="showHover demo"><a href="https://gdl003-burger-queen-front-end.brendavielmas.now.sh/"><p>Demo</p></a></div>
+                    <div className="showHover demo"><a href="https://piano-app-003.firebaseapp.com/"><p>Demo</p></a></div>
                     <div className="showHover github"><a href="https://github.com/BrendaVielmas/GDL003-personal-project"><p>Github</p></a></div>
                   </div>
                 </div>
